8bb5694b4b9f8425c790078672dd2cde6e942369
mangueBit <alm3@cin.ufpe.br>
2018-01-07 12:25:40 -0300

    simple calculator class

---
+++ classes.py
@@ -0,0 +1,22 @@
+class calculator:
+
+	def addition(x,y):
+		add = x + y
+		print(add)
+
+	def subtraction(x,y):
+		sub = x - y
+		print(sub)
+
+	def multiplication(x,y):
+		mul = x * y
+		print(mul)
+
+	def division(x,y):
+		div = x / y
+		print(div)
+
+calculator.addition(3,5)
+calculator.subtraction(3,5)
+calculator.multiplication(3,5)
+calculator.division(3,5)
