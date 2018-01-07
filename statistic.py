8e6797e82b64ee783425a0b1b7f052f33d27d016
mangueBit <alm3@cin.ufpe.br>
2018-01-07 13:01:25 -0300

    statistics module

---
+++ statistic.py
@@ -0,0 +1,11 @@
+import statistics
+
+example_list = [9,8,7,6,5,4,3,2,1]
+
+x = statistics.mean(example_list)
+y = statistics.median(example_list)
+z = statistics.variance(example_list)
+
+print(x)
+print(y)
+print(z)
