fd82120238d38df4fb2cebdb2bb824b670337ce1
mangueBit <alm3@cin.ufpe.br>
2018-01-07 12:46:39 -0300

    simple module import

--- epicthing.py
+++ epicthing.py
@@ -1,8 +1,10 @@
 def epic():
 	print('wow this is great!')
 
-'''... so we put this if for the
+'''... so we put this IF for the
 		module to not be printed/executed		
 '''
+
+#this IF kind asks: 'is this the main file ?'
 if __name__ == '__main__':
 	print('such great module')
